fa24a915940b827764eb9aa389aae6f373de4379
geertza <geertza@gmail.com>
2021-01-30 01:05:04 -0700

    s

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');gig
 const path = require('path');
 const app = express();
 
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+// app.get('/api/customers', cors(), (req, res) => {
+//   const customers = [
+//     {id: 1, firstName: 'John', lastName: 'Doe'},
+//     {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+//     {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+//   ];
 
-  res.json(customers);
-});
+//   res.json(customers);
+// });
 // Priority serve any static files.
 // app.use(express.static(path.resolve(__dirname, './client/build')));
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World      yeah boi')
 })
 const port = 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port, console.log(`Server running on port ${port}`));
